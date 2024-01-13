9c38eb4ec6133bef5e3bb0a6066c6beb2c19a433
PC <PC@RICHARD>
2024-01-12 23:11:22 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void sumaNumeros(){
+
+                int num = 2;
+                int num2 = 3;
+
+                int resultado = num + num2;
+
+                Console.WriteLine(resultado);
+            }
+
+
+        }
+    }
+
+}
